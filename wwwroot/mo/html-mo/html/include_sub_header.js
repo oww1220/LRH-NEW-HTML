fc8b34b3ea2b9fd6113d645cd0e0c30c01d01387
yhsky <yhsky@mz.co.kr>
2020-09-25 19:34:05 +0900

    이벤트 및 당첨자 리스트 완성

--- wwwroot/mo/html-mo/html/include_sub_header.js
+++ wwwroot/mo/html-mo/html/include_sub_header.js
@@ -5,23 +5,20 @@ $(function(){
         <!--header 컴포넌트-->
         <header class="header">
             <div class="header-top">
-                <h1><a href="#none">해더부분</a></h1>
-                <button type="button" class="btn-drawer" >
-                    <i class="icon">
-                        <i class="line line-1"></i>
-                        <i class="line line-2"></i>
-                        <i class="line line-3"></i>
-                    </i>
+                <h1 class="header-logo"><a href="#none"><i class="icon logo">롯데렌터카</i></a></h1>
+                <button type="button" class="btn-mypage">
+                    <i class="icon mypage">마이페이지</i>
+                </button>
+                <button type="button" class="btn-drawer" data-target="drawer" data-on="true" data-sort="none">
+                    <i class="icon draw">메뉴열기</i>
                 </button>
             </div>
             <nav class="header-nav">
-                <a href="#">홈</a>
-                <a href="#">빠른출고</a>
-                <a href="#">프로모션</a>
-                <a href="#" class="active">특가모아보기</a>
-                <a href="#">이벤트</a>
-                <a href="#">고객후기</a>
-                <a href="#">신차장TV</a>
+                <a href="#" class="active">#카라이프</a>
+                <a href="#">단기예약</a>
+                <a href="#">신차장</a>
+                <a href="#">중고차</a>
+                <a href="#">고객지원</a>
             </nav>
         </header>
         <!--//header 컴포넌트-->
