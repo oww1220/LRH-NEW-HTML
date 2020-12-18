f935b1b3f17fa0fdb94740e7074196a4afd8e7fc
Park Ji Hyun <endless103@gmail.com>
2020-12-18 19:01:46 +0900

    웹 영문) 1. 마이페이지 - my reservaiton 목록, 내역없음, 상세,취소 - modify personal infomaition 입력, 결과, 탈퇴 - my branch, 내역없음 - check member point (point used / point exchange, 내역없음)
    2. footer
    - footerMenu_about

--- wwwroot/pc/html-pc/eng_html/include_mypage.js
+++ wwwroot/pc/html-pc/eng_html/include_mypage.js
@@ -5,46 +5,33 @@ $(function(){
 	<div class="myInfos-area">
 		<div class="box-between">
 			<h3 class="txt">
-				<strong class="name">김롯데님</strong>
-				<span>일반등급</span>
-			</h3>
-			<div class="btns">
-				<a href="#" class="btn-goLink arrow-r">운전면허증</a>
-				<a href="#" class="btn-goLink arrow-r">결제카드</a>
-			</div>
+				<p class="greeting">Hello</p>
+				<strong class="name">kimLOTTE (ID123)</strong>
+			</h3>						
+			<ul class="item-list">
+				<li>	
+					<span class="icon myPannel07"></span>
+					<div class="txt-box">
+						<p>L.POINT</p>
+						<span>General</span>
+					</div>
+				</li>
+				<li>	
+					<span class="icon myPannel02"></span>
+					<div class="txt-box">
+						<p>Point</p>
+						<span>469,100</span>
+					</div>
+				</li>
+				<li>	
+					<span class="icon myPannel05"></span>
+					<div class="txt-box">
+						<p>Rental records</p>
+						<span>2</span>
+					</div>
+				</li>
+			</ul>
 		</div>
-		<ul class="item-list">
-			<li>	
-				<span class="icon myPannel01"></span>
-				<p>L.POINT</p>
-				<a href="#" class="underline">12,300</a>
-			</li>
-			<li>	
-				<span class="icon myPannel02"></span>
-				<p>렌터카 포인트</p>
-				<a href="#" class="underline">469,100</a>
-			</li>
-			<li>	
-				<span class="icon myPannel03"></span>
-				<p>렌터카 쿠폰</p>
-				<a href="#" class="underline">2장</a>
-			</li>
-			<li>	
-				<span class="icon myPannel04"></span>
-				<p>멤버쉽 쿠폰</p>
-				<a href="#" class="underline">2장</a>
-			</li>
-			<li>	
-				<span class="icon myPannel05"></span>
-				<p>내차팔기 상담</p>
-				<a href="#" class="underline">1건</a>
-			</li>
-			<li>	
-				<span class="icon myPannel06"></span>
-				<p>렌터카 상담</p>
-				<a href="#" class="underline">1건</a>
-			</li>
-		</ul>
 	</div>
 	`;
 	
