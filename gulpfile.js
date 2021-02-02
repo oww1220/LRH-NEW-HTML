22c52e64859ca8f238d44c92285e6a210dd9db7e
이동걸 <oww@mz.co.kr>
2021-02-02 15:09:47 +0900

    걸프 파일 재설정

--- gulpfile.js
+++ gulpfile.js
@@ -37,6 +37,7 @@ gulp.task('uglify', function (done) {
   ); //dist 폴더에 저장
 });
 
+/*일반*/
 gulp.task('sassPC', function () {
   return gulp
     .src('wwwroot/pc/resources-pc/scss/**/*.scss')
@@ -162,6 +163,133 @@ gulp.task('buildMO', function () {
   );
 });
 
+
+/*제휴사*/
+gulp.task('sassPC-co', function () {
+  return gulp
+    .src('wwwroot/cooperation/pc/resources-pc/scss/**/*.scss')
+    .pipe(plumber(plumberOption))
+    .pipe(
+      sourcemaps.init({
+        loadMaps: true,
+      })
+    )
+    .pipe(
+      sass({
+        outputStyle: 'expanded', //[nested, compact, expanded, compressed]
+        indentType: 'tab',
+        indentWidth: 1,
+      }).on('error', sass.logError)
+    )
+    .pipe(
+      autoprefixer({
+        browsers: autoprefixBrowsers,
+        cascade: true,
+      })
+	)
+    .pipe(sourcemaps.write('../maps'))
+    .pipe(gulp.dest('wwwroot/cooperation/pc/resources-pc/styles'))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('buildPC-co', function () {
+  return gulp
+    .src('wwwroot/cooperation/pc/resources-pc/scss/**/*.scss')
+    .pipe(plumber(plumberOption))
+    .pipe(
+      sass({
+        outputStyle: 'compressed', //[nested, compact, expanded, compressed]
+      }).on('error', sass.logError)
+    )
+    .pipe(
+      autoprefixer({
+        browsers: autoprefixBrowsers,
+        cascade: true,
+      })
+    )
+    .pipe(gulp.dest('wwwroot/cooperation/pc/resources-pc/styles/dist'))
+    .pipe(browserSync.reload({ stream: true }))
+    .on('end', function () {
+      console.log('-------- appned css --------');
+    });
+});
+
+gulp.task('sassMO-co', function () {
+  return (
+    gulp
+      .src('wwwroot/cooperation/mo/resources-mo/scss/**/*.scss')
+      .pipe(plumber(plumberOption))
+      .pipe(
+        sourcemaps.init({
+          loadMaps: true,
+        })
+      )
+      .pipe(
+        sass({
+          outputStyle: 'expanded', //[nested, compact, expanded, compressed]
+          indentType: 'tab',
+          indentWidth: 1,
+        }).on('error', sass.logError)
+      )
+      .pipe(
+        autoprefixer({
+          browsers: autoprefixBrowsers,
+          cascade: true,
+        })
+      )
+      /*
+		.pipe(pxtorem({
+			propList: ['*', '!'],
+			rootValue: 16,
+			replace: false,
+			minPixelValue: 2,
+			mediaQuery: true
+		}))
+		*/
+      .pipe(sourcemaps.write('../maps'))
+      .pipe(gulp.dest('wwwroot/cooperation/mo/resources-mo/styles'))
+      .pipe(browserSync.reload({ stream: true }))
+  );
+});
+
+gulp.task('buildMO-co', function () {
+  return (
+    gulp
+      .src('wwwroot/cooperation/mo/resources-mo/scss/**/*.scss')
+      .pipe(plumber(plumberOption))
+      .pipe(
+        sass({
+          outputStyle: 'compressed', //[nested, compact, expanded, compressed]
+        }).on('error', sass.logError)
+      )
+      .pipe(
+        autoprefixer({
+          browsers: autoprefixBrowsers,
+          cascade: true,
+        })
+      )
+      /*
+		.pipe(pxtorem({
+			propList: ['*', '!'],
+			rootValue: 16,
+			replace: false,
+			minPixelValue: 2,
+			mediaQuery: true
+		}))
+		.pipe(modifyCssUrls({
+			modify: function (url, filePath) {
+				return url.replace('../images/', '../../images/app/');
+			},
+		}))
+		*/
+      .pipe(gulp.dest('wwwroot/cooperation/mo/resources-mo/styles/dist'))
+      .pipe(browserSync.reload({ stream: true }))
+      .on('end', function () {
+        console.log('-------- appned css --------');
+      })
+  );
+});
+
 gulp.task('watch', function () {
   browserSync.init({
     //logLevel: "debug",
@@ -181,11 +309,22 @@ gulp.task('watch', function () {
     'wwwroot/mo/resources-mo/scss/**/*.scss',
     gulp.series('sassMO', 'buildMO')
   );
+
+  /*제휴사*/
+  gulp.watch(
+    'wwwroot/cooperation/pc/resources-pc/scss/**/*.scss',
+    gulp.series('sassPC-co', 'buildPC-co')
+  );
+  gulp.watch(
+    'wwwroot/cooperation/mo/resources-mo/scss/**/*.scss',
+    gulp.series('sassMO-co', 'buildMO-co')
+  );
+
   gulp.watch('wwwroot/**/*.html').on('change', browserSync.reload);
   gulp.watch('wwwroot/**/*.js').on('change', browserSync.reload);
 });
 
 gulp.task(
   'default',
-  gulp.series('sassPC', 'buildPC', 'sassMO', 'buildMO', 'watch')
+  gulp.series('sassPC', 'buildPC', 'sassMO', 'buildMO', 'sassPC-co', 'buildPC-co', 'sassMO-co', 'buildMO-co', 'watch')
 );
